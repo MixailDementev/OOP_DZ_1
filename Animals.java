f55b4d6d112cb7186736325631c172acff3d057c
MDementev <vanneste1207@gmail.com>
2023-01-22 20:10:06 +0300

    DZ2

---
+++ Animals.java
@@ -0,0 +1,26 @@
+package OOP_DZ_1;
+
+public abstract class Animals {
+    private String Animals;
+    private String AnimalsName;
+
+
+    public void setAnimals(String animals){
+        this.Animals = animals;
+    }
+    public void setAnimalsName(String animalsname){
+        this.AnimalsName = animalsname;
+    }
+
+
+    public String getAnimals(){
+        return Animals;
+    }
+    public String getAnimalsName(){
+        return AnimalsName;
+    }
+
+
+
+
+}
