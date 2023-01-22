0ef1858e81e17e4874cf960ce3c27096d0f1cd65
MDementev <vanneste1207@gmail.com>
2023-01-22 20:11:10 +0300

    DZ2

--- MainApp.java
+++ MainApp.java
@@ -1,24 +1,31 @@
 package OOP_DZ_1;
 
 public class MainApp {
-    
-    public static void main(String[] args) {
-        Persona Father = new Persona("Дубов Павел Владимирович");
-        Persona Son = new Persona("Дубов Сергей Павлович");
-        Persona Daughter = new Persona("Иванова Ольга Павловна");
-        Persona Grandson = new Persona("Дубов Кирилл Сергеевич");
-        Persona Grandson_2 = new Persona("Иванов Иван Иванович");
-        Persona Granddauter = new Persona("Дубова Людмила Сергеевна");
-        Persona Granddauter_2 = new Persona("Березова Екатерина Ивановна");
-        Persona Grandgranddauter = new Persona("Березова Тамара Сергеевна");
-        Father.addName(Son);
-        Father.addName(Daughter);
-        Son.addName(Grandson);
-        Son.addName(Granddauter);
-        Daughter.addName(Granddauter_2);
-        Daughter.addName(Grandson_2);
-        Granddauter_2.addName(Grandgranddauter);
 
-        Family.treeView(Father,0);
+    public static void main(String[] args) {
+
+        Persona father = new Persona("male", "John", 30, "Lib1", "dog");
+        Persona son = new Persona("male", "Pit", 8, "Lib2", "cat");
+
+        father.personaLib.AddBook("Достоевский", "Идиот", "русский");
+        son.personaLib.AddBook("Народное творчество", "Азбука", "русский");
+
+        father.PersAnimal.AddAnimal("бульдог", "Граф");
+        son.PersAnimal.AddAnimal("шотландская", "Мурка");
+
+        father.addName(son);
+        father.Voice();
+
+        System.out.println("*".repeat(15));
+        View.treeView(father, 0);
+        System.out.println("*".repeat(15));
+        View.bookView(father);
+        View.bookView(son);
+
+        System.out.println("*".repeat(15));
+        View.animalView(father);
+        View.animalView(son);
+
+
     }
 }
